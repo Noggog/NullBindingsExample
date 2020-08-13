16f84f8da7ec46b3e6e4dafcedb9ea3b2945f3c6
Justin Swanson <justin.c.swanson@gmail.com>
2020-08-13 09:53:26 -0500

    Example

---
+++ NullBindings/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Noggog.WPF;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Reactive.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace NullBindings
+{
+    public class MainViewModel : ViewModel
+    {
+        [Reactive]
+        public SubObject MyObject { get; set; } = new SubObject();
+
+        public IReactiveCommand SetToNullCommand { get; }
+
+        public MainViewModel()
+        {
+            SetToNullCommand = ReactiveCommand.Create(() => MyObject = null);
+        }
+    }
+}
---
+++ NullBindings/SubObject.cs
@@ -0,0 +1,14 @@
+﻿using Noggog.WPF;
+using ReactiveUI.Fody.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NullBindings
+{
+    public class SubObject : ViewModel
+    {
+        [Reactive]
+        public string MyText { get; set; } = "Hello World";
+    }
+}
